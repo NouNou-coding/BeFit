673478c03d220be5efab49e8edd9c30f5751293b
francis127-cmd <francois.maarbes@gmail.com>
2025-07-14 13:05:56 +0300

    Update cookie-consent.js

--- BeFit-Folder/auth/cookie-consent.js
+++ BeFit-Folder/auth/cookie-consent.js
@@ -1,8 +1,7 @@
-// Cookie functions (keep these)
 function setCookie(name, value, days) {
   const expires = new Date();
   expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Lax;Secure`;
 }
 
 function getCookie(name) {
